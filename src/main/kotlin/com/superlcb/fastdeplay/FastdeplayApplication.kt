35cb5913498048f2fabc9a952a9297e7b1733294
liuchengbiao <liuchengbiao@bytedance.com>
2021-09-25 10:05:09 +0800

    Initial commit

---
+++ src/main/kotlin/com/superlcb/fastdeplay/FastdeplayApplication.kt
@@ -0,0 +1,11 @@
+package com.superlcb.fastdeplay
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FastdeplayApplication
+
+fun main(args: Array<String>) {
+    runApplication<FastdeplayApplication>(*args)
+}
---
+++ src/test/kotlin/com/superlcb/fastdeplay/FastdeplayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.superlcb.fastdeplay
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FastdeplayApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
